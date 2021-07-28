1d16c95dd97acc3704d6ae5b8d01736526f26b43
junni11 <wkszlf98@naver.com>
2021-07-29 04:10:43 +0900

    add icon on reset

--- src/components/reset-button/resetModal.tsx
+++ src/components/reset-button/resetModal.tsx
@@ -1,5 +1,7 @@
 import React, { useState, useRef, useEffect } from 'react';
 import 'components/reset-button/style/resetModal.css';
+import imgPw from 'resources/images/scope2.png';
+import imgText from 'resources/images/scope.png';
 import $ from 'jquery';
 
 type ModalProps = {
@@ -9,9 +11,9 @@ type ModalProps = {
 };
 
 const ResetModal = (props: ModalProps) => {
+  const [lookPw, setLookPw] = useState(false);
   const [pw, setPw] = useState('');
   const { open, close, reset } = props;
-
 
   const onConfirm = (event: React.MouseEvent<HTMLButtonElement>) => {
     event.preventDefault();
@@ -19,16 +21,16 @@ const ResetModal = (props: ModalProps) => {
       alert(`채팅창이 초기화 되었습니다.`);
       reset();
       closeWithPw();
-    }
-    else {
+    } else {
       alert(`암호가 틀렸습니다.`);
     }
   };
 
   const closeWithPw = () => {
     close();
+    setLookPw(false);
     setPw('');
-  }
+  };
 
   const pwHandler = (event: any) => setPw(event.target.value);
 
@@ -46,6 +48,11 @@ const ResetModal = (props: ModalProps) => {
     }
   };
 
+  const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+    setLookPw(!lookPw);
+  };
+
   return (
     // 모달이 열릴때 openModal 클래스가 생성된다.
     <div className={open ? 'open_modal modal' : 'modal'} onKeyDown={onEscPress}>
@@ -53,7 +60,11 @@ const ResetModal = (props: ModalProps) => {
         <section>
           <header>
             채팅창 초기화
-            <button type='button' className='modal__close_btn' onClick={closeWithPw}>
+            <button
+              type='button'
+              className='modal__close_btn'
+              onClick={closeWithPw}
+            >
               {' '}
               &times;{' '}
             </button>
@@ -61,12 +72,23 @@ const ResetModal = (props: ModalProps) => {
           <main>
             <div className='modal__input_pw'>
               <input
-                type='password'
+                type={lookPw ? 'text' : 'password'}
                 placeholder='암호'
                 value={pw}
                 onChange={pwHandler}
                 onKeyDown={onEnterPress}
               />
+              <button
+                type='button'
+                onClick={onClick}
+                className='modal__input_look_pw_btn'
+              >
+                <img
+                  src={lookPw ? imgText : imgPw}
+                  alt='LookPassword'
+                  className='modal__input_look_pw_btn__img'
+                />
+              </button>
             </div>
           </main>
           <footer>
